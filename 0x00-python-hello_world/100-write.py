727c7795d2fd22b699fd38c1d4d2470c4272423e
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-01-02 16:46:20 -0800

    A Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-def main():
-    message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+# Write the message to stderr
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 
-    # Write the message to stderr
-    sys.stderr.write(message + '\n')
-
-    # Exit with status code 1
-    sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+# Exit with status code 1
+sys.exit(1)
