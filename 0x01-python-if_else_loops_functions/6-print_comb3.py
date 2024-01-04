2c5900c1d334e5bdebfb013bde56d5d9d793ea73
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-01-03 18:59:26 -0800

    A Program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+output = ""
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
         if tens_digit == 8:
-            print("{:02d} ".format(tens_digit * 10 + ones_digit), end='')
+            output += "{:02d} ".format(tens_digit * 10 + ones_digit)
         else:
-            print("{:02d}, ".format(tens_digit * 10 + ones_digit), end='')
+            output += "{:02d}, ".format(tens_digit * 10 + ones_digit)
+output = output.rstrip(', ')
+print(output)
