bb889402c35e584c06bcac28f76393b95347f548
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-01-05 23:52:18 -0800

    A program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
+from calculator_1 import add, sub, mul, div
 result_add = add(a,b)
 result_sub = sub(a,b)
 result_mul = mul(a,b)
