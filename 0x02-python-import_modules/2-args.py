8bcc1ef12779904506b49a4fa2de2002a955990c
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-01-06 14:56:05 -0800

    A Program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import sys
+if __name__ = '__main__':
+    import sys
 
-iterable = sys.argv
-iterable = iterable[1:]
+    iterable = sys.argv
+    iterable = iterable[1:]
 
-def enumerate(iterable, start=1):
-    n = start
-    if len(sys.argv)-1 == 1:
-        print('{} argument:'.format(len(sys.argv)-1))
-    else:
-        print('{} arguments:'.format(len(sys.argv)-1))
-    for elem in iterable:
-        print('{}: {}'.format(n, elem), end='\n')
-        n += 1
-enumerate(iterable)
+    def enumerate(iterable, start=1):
+        n = start
+        if len(sys.argv)-1 == 1:
+            print('{} argument:'.format(len(sys.argv)-1))
+        else:
+            print('{} arguments:'.format(len(sys.argv)-1))
+            for elem in iterable:
+                print('{}: {}'.format(n, elem), end='\n')
+            n += 1
+
+    enumerate(iterable)
