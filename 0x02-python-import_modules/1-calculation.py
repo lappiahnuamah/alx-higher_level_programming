381c42417e4d09c165b2fb84f74c61e9437f3c0b
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-01-08 09:27:22 -0800

    A program that imports functions from a file and does some Mahts

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-    result_add = add(a,b)
-    result_sub = sub(a,b)
-    result_mul = mul(a,b)
-    result_div = div(a,b)
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
     print('{} + {} = {}'.format(a, b, result_add))
     print('{} - {} = {}'.format(a, b, result_sub))
     print('{} * {} = {}'.format(a, b, result_mul))
